45f25e7dec8d93e2221447181c28c6560d3fd689
Mohamed <Mona.Mohamed@seattlecolleges.edu>
2019-07-02 17:47:31 -0700

    Assignment 1 final submission

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 //Hello World in node.js
-//HWtest1.js
+
 
 
 
 //This line gets the http module and saves it to the variable http
 //The http module allows node.js to transfer data over http
 var http = require('http');
-
+const fs = require('fs');
 http.createServer((req,res) => {
   const path = req.url.toLowerCase();
   switch(path) {
   case '/':
-    const fs = require('fs');
+    
     fs.readFile('public/home.html', (err, data) => {
       if (err) return console.error(err);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data.toString());
     });
   break;
-  case '/about':
+  /* case '/about':
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('About Page');
-  break;
+  break; */
+
+  case '/home':
+      fs.readFile('public/home.html', (err, data) => {
+      if (err) return console.error(err);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data.toString());
+    });
     
-    case '/history':
-    const fs1 = require('fs1');
-    fs1.readFile('public/history.html', (err, data) => {
+    case '/about':
+    //const fs1 = require('fs1');
+    fs.readFile('public/history.html', (err, data) => {
     if (err) return console.error(err);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(data.toString());
   });
     break;  
   case '/why':
-    const fs2 = require('fs2');
-     fs2.readFile('public/why.html', (err,data) => {
+    //const fs2 = require('fs2');
+     fs.readFile('public/why.html', (err,data) => {
      if (err) return console.error(err);
      res.writeHead(200, {'Content-Type': 'text/html'});
      res.end(data.toString());
     });
   break;
   case '/tutorial':
-    const fs3 = require('fs3');
-    fs3.readFile('public/tutorial.html', (err, data) => {
+   // const fs3 = require('fs3');
+    fs.readFile('public/tutorial.html', (err, data) => {
     if (err) return console.error(err);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(data.toString());
